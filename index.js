1c8890bc347a762563bb52a8030c5e986ad1657f
stephen oshyn <scoshyn@gmail.com>
2023-08-26 09:40:27 -0700

    lab complete

---
+++ index.js
@@ -0,0 +1,34 @@
+let n;
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return (n += 1);
+}
+
+function decrement(n) {
+    return (n -= 1);
+}
+
+function makeInt(n) {
+    n = parseInt(n, 10);
+    return n;
+}
+
+function preserveDecimal(n) {
+    n = parseFloat(n);
+    return n;
+}
